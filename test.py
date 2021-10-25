eeea756acfde444fc7324033a17003e1ff390927
Pawan Rai <42629885+pawan9116@users.noreply.github.com>
2021-10-25 21:58:29 +0530

    Fixed bug

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestShoppingCart(unittest.TestCase):
 	def setUp(self):
 		self.cart=ShoppingCart.ShoppingCart()
 	def test1(self):
-		msg = self.cart.additems(4)
+		msg = self.cart.additems(3)
 		self.assertEqual(msg,'Successfully added 3 items to cart!')
        
 		
